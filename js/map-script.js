f0efee776c7b944e93c12892d438b2951d9c768e
Simon Martín <oleurud@gmail.com>
2019-01-15 19:09:15 +0100

    using default markers

--- js/map-script.js
+++ js/map-script.js
@@ -1,21 +1,21 @@
-	
-	'use strict';	
-	
+
+	'use strict';
+
 	// CHECK WINDOW RESIZE
 	var is_windowresize = false;
 	$(window).resize(function(){
 		is_windowresize = true;
 	});
-	
-	
+
+
 	//INITIALIZE MAP
 	function initialize() {
-		
+
 		//DEFINE MAP OPTIONS
 		//=======================================================================================
   		var mapOptions = {
-    		zoom: 18,		
-			mapTypeId: google.maps.MapTypeId.ROADMAP,	
+    		zoom: 18,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
     		center: new google.maps.LatLng(43.1939864,-4.0556559),
 			panControl: true,
   			zoomControl: true,
@@ -24,17 +24,17 @@
   			streetViewControl: true,
   			overviewMapControl: true,
 			//rotateControl:true,
-			
+
   		};
 
 		//CREATE NEW MAP
 		//=======================================================================================
   		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-		
+
 		//MARKER ICON
 		//=======================================================================================
 		//var image = 'facebook30.svg';
-		
+
 		//ADD NEW MARKER
 		//=======================================================================================
   		/*var marker = new google.maps.Marker({
@@ -43,101 +43,101 @@
     		title: 'Click to zoom',
 			icon: image
   		});
-		
+
 		var marker1 = new google.maps.Marker({
     		position: new google.maps.LatLng(-12.042559, -77.027426),
    		 	map: map,
     		title: 'Click to zoom'
   		});*/
-		
-		
+
+
 		//ADD NEW MARKER WITH LABEL
 		//=======================================================================================
-		var marker1 = new MarkerWithLabel({
+		var marker1 = new google.maps.Marker({
        		position: new google.maps.LatLng(43.1939864,-4.0556559),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle medium-size" style="background-color:#FFF; border:1px solid #f0394d"><i class="de-icon-heart" style="color:#f0394d"></i></div>',
        		labelAnchor: new google.maps.Point(29, 20),
        		labelClass: "labels" // the CSS class for the label
      		});
-    
-		var marker2 = new MarkerWithLabel({
+
+		var marker2 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.046040, -77.029269),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#6a6a6a"><i class="de-icon-taxi"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
      		});
-			
-		var marker3 = new MarkerWithLabel({
+
+		var marker3 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.045909, -77.031712),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#67a80e"><i class="de-icon-food"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
      		});
-			
-		var marker4 = new MarkerWithLabel({
+
+		var marker4 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.046617, -77.030567	),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#c89d1b"><i class="de-icon-coffee"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
      		});
-			
-		var marker5 = new MarkerWithLabel({
+
+		var marker5 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.045857, -77.032538),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#f0394d"><i class="de-icon-basket"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
-     		});	
-		
-		var marker6 = new MarkerWithLabel({
+     		});
+
+		var marker6 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.046053, -77.028732),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#f6700e"><i class="de-icon-paper-plane"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
-     		});	
-			
-		var marker7 = new MarkerWithLabel({
+     		});
+
+		var marker7 = new google.maps.Marker({
        		position: new google.maps.LatLng(-12.045363, -77.029939),
        		draggable: false,
        		raiseOnDrag: false,
        		icon: ' ',
-       		map: map, 
+       		map: map,
          	labelContent: '<div class="de-icon circle small-size" style="background-color:#0d9a48"><i class="de-icon-tree"></i></div>',
        		labelAnchor: new google.maps.Point(0, 0),
        		labelClass: "labels" // the CSS class for the label
-     		});			
+     		});
     	//marker.setMap( map );
-		
-		
+
+
 		//INFO WINDOWS
 		//=======================================================================================
 		var contentString = '<div>'+
 		'LA CASONA DE LAS FRAGUAS';
       	'</div>';
-		
+
 		var contentString1 = '<div>'+
 		'WEDDING PARTY';
       	'</div>';
@@ -145,17 +145,17 @@
  	 	var infowindow = new google.maps.InfoWindow({
       		content: contentString
   		});
-		
+
 		var infowindow1 = new google.maps.InfoWindow({
       		content: contentString1
   		});
-		
-		
-		
+
+
+
 		//OPEN INFO WINDOWS ON LOAD
 		//=======================================================================================
   		infowindow.open(map,marker1);
-		
+
 		//ON CLICK MARKER, OPEN INFO WINDOWS
 		//=======================================================================================
 		google.maps.event.addListener(marker1, 'click', function() {
@@ -169,22 +169,22 @@
     		map.setCenter(marker.getPosition());
 			infowindow.open(map,marker);
   		});
-		
+
 		google.maps.event.addListener(marker1, 'click', function() {
    	 		map.setZoom(17);
     		map.setCenter(marker.getPosition());
 			infowindow1.open(map,marker1);
   		});
-		
+
 		google.maps.event.addListener(marker2, 'click', function() {
    	 		map.setZoom(17);
     		map.setCenter(marker.getPosition());
 			infowindow1.open(map,marker2);
   		});*/
-		
+
 		//ON BOUND EVENTS AND WINDOW RESIZE
 		//=======================================================================================
-		google.maps.event.addListener(map, 'bounds_changed', function() {  		
+		google.maps.event.addListener(map, 'bounds_changed', function() {
 			if (is_windowresize)
 			{
 				//map.setCenter(marker.getPosition());
@@ -198,5 +198,5 @@
 
 	// LOAD GMAP
 	google.maps.event.addDomListener(window, 'load', initialize);
-	
-	
+
+
